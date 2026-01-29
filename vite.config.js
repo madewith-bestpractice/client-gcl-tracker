91feb08d2049759cf79bf81a9eb3094f0385d4ed
bp-brendan <brendan@madewithbestpractice.com>
2026-01-29 16:40:45 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from "@vitejs/plugin-react";
 import { cloudflare } from "@cloudflare/vite-plugin";
 
 export default defineConfig({
+  build: {
+    target: "es2022",
+  },
   plugins: [react(), cloudflare()],
 });
